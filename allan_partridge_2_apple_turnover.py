373ac5756cc94b7e96c7cb2014f4db2423cde14c
iian armas <armas.cav@gmail.com>
2021-08-04 20:39:19 +0800

    Create allan_partridge_2_apple_turnover.py

---
+++ allan_partridge_2_apple_turnover.py
@@ -0,0 +1,15 @@
+# Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+
+# variation 1
+def apple(x):
+    if pow(int(x), 2) > 1000:
+        return 'it\'s hotter than the sun'
+    return 'okay'
+  
+ # variation 2
+def apple(x):
+    return "It's hotter than the sun!!" if pow(int(x), 2) > 1000 else "Help yourself to a honeycomb Yorkie for the glovebox."
+  
+ # variation 3
+def apple(x):
+    return ("Help yourself to a honeycomb Yorkie for the glovebox.","It's hotter than the sun!!")[int(x)**2>1000]
